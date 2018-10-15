bfc18d56f1ef2609e684579c2f897bcc3c3c0342
TI-130 <Tyronemonitor@gmail.com>
2018-10-14 18:40:16 -0700

    Update MyArrayList.java

--- chap_arrays_and_strings/MyArrayList.java
+++ chap_arrays_and_strings/MyArrayList.java
@@ -1,11 +1,11 @@
 public class MyArrayList {
-  int   size;
-  int[] cont;
-  final int initSize = 10;
+  private int   size;
+  private Object[] cont;
+  private final int initSize = 10;
   
   public MyArrayList() {
     size = initSize;
-    cont = new Array[Object](initSize);
+    cont = new Object[initSize];
   }
   
   public boolean add(Object obj) {
@@ -17,6 +17,7 @@ public class MyArrayList {
     }
     doubleTheSize();
     add(obj);
+    return true;
   }
   
   public boolean add(int index, Object obj) {
@@ -26,26 +27,59 @@ public class MyArrayList {
        cont[i] = cont[i-1];
     }
     cont[index] = obj;
+    return true;
   }
   
   public boolean remove(Object obj) {
-    
+    return false;
   }
   
   public boolean remove(int index) {
-    
+    return false;
+  }
+  
+  public void showContent() {
+      for (int i = 0; i < cont.length; i++) {
+          System.out.println(cont[i]);
+      }
   }
   
   
   public Object get(int index) {
-     
+     return cont[index];
   }
   
   private void doubleTheSize() {
-    
+    size *= 2;
+    Object[] newCont = new Object[cont.length * 2];
+    for (int i = 0; i < cont.length; i++) {
+        newCont[i] = cont[i];
+    }
+    cont = newCont;
   }
   
-  public void main(String[] args) {
-    System.out.println("Hello"); 
+  public static void main(String[] args) {
+      
+    MyArrayList al = new MyArrayList();
+    al.add(1);
+    al.add(2);
+    al.add("3");
+    al.add(false);
+    al.add(al);
+    // al.add("6");
+    // al.add("7");
+    // al.add("8");
+    // al.add("9");
+    // al.add("10");
+    // al.add("11");
+    // al.add("12");
+    // al.add("13");
+    // al.add("14");
+    // al.add("15");
+    // al.add("16");
+    // al.add("17");
+    // al.add("18");
+    
+    al.showContent();
   }
 }
